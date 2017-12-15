5d64c95a8e04232131a95333bf4d7c187f6d8673
jennclee <8753352+jennclee@users.noreply.github.com>
2017-12-14 16:22:41 -0800

    Add methods to add and retrieve individual users and listings

---
+++ database/index.js
@@ -0,0 +1,99 @@
+// const Promise = require('bluebird');
+const path = require('path');
+
+const models = require('express-cassandra');
+
+const dbConfig = {
+  contactPoints: ['127.0.0.1'],
+  protocolOptions: { port: 9042 },
+  keyspace: 'testing',
+  queryOptions: { consistency: models.consistencies.one },
+};
+
+const ormConfig = {
+  defaultReplicationStrategy: {
+    class: 'SimpleStrategy',
+    replication_factor: 1,
+  },
+  migration: 'safe',
+};
+
+models.setDirectory(path.join(__dirname, '/models')).bindAsync({
+  clientOptions: dbConfig,
+  ormOptions: ormConfig,
+});
+
+module.exports = {
+  addUser: (username) => {
+    return new Promise((resolve, reject) => {
+      const user = new models.instance.users({
+        username,
+      });
+
+      user.save((err, res) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(res);
+        console.log('Saved user!');
+      });
+    });
+  },
+  findUser: (username) => {
+    return new Promise((resolve, reject) => {
+      models.instance.users.findOne({ username }, (err, user) => {
+        if (err) reject(err);
+        resolve(user);
+      });
+    });
+  },
+  addListing: (
+    location,
+    title,
+    description,
+    price,
+    maxGuests,
+    roomType,
+    bedrooms,
+    bathrooms,
+    beds,
+    overallRating,
+    accomodationType,
+    hostID,
+    availabilityPreferences
+  ) => {
+    return new Promise((resolve, reject) => {
+      const listing = new models.instance.listings({
+        location,
+        title,
+        description,
+        price,
+        maxGuests,
+        roomType,
+        bedrooms,
+        bathrooms,
+        beds,
+        overallRating,
+        accomodationType,
+        hostID,
+        availabilityPreferences,
+      });
+
+      listing.save((err, res) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(res);
+        console.log('Saved listing!');
+      });
+    });
+  },
+  findListing: (listingId) => {
+    return new Promise((resolve, reject) => {
+      models.instance.listings.findOne({ listingId }, (err, user) => {
+        if (err) reject(err);
+        resolve(user);
+      });
+    });
+  },
+};
